e9137ff0233bc70e02322e67e27eec21aaac8d7d
railek <jean@railek.com>
2022-11-08 20:48:17 +0200

    style: scroll fade animation

--- components/elements/card/card.js
+++ components/elements/card/card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ScrollFade from '@/components/elements/scroll-fade';
+
 import { StyledCard } from './card.styled';
 
 export default function Card({ children }) {
-  return <StyledCard>{children}</StyledCard>;
+  return (
+    <ScrollFade threshold={0.2}>
+      <StyledCard>{children}</StyledCard>
+    </ScrollFade>
+  );
 }
 
 Card.propTypes = {
